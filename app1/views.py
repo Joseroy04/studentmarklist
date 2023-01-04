e079ab751d58efc9e4bef1bdc343beb687afb643
Joseroy04 <88181888+Joseroy04@users.noreply.github.com>
2023-01-04 19:53:21 +0530

    final 1

--- app1/views.py
+++ app1/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render,redirect
 from .forms import loginForm
 # Create your views here.
-
+from .models import studentData,Deportment,Pedagogy
 def home (request ):
     form = loginForm()
     return render(request,"home.html",{"form":form})
 
 def marklist (request ):
     if request.method == "POST":
-        forms = loginForm(request.POST)
-        if forms.is_valid:
-            print(forms)
-            stuId  = forms.cleaned_data['studentId']
-            dob  = forms.cleaned_data['dob']
-            
-            return render(request,"marklist.html",{"id":stuId,"dob":dob})
-        return render(request,"marklist.html",{})
+        form = loginForm(request.POST)
+        if form.is_valid():
+            # print(forms)
+            stuId  = form.cleaned_data['studentId']
+            dob  = form.cleaned_data['dob']
+            print(stuId,dob)
+            dat =  list(studentData.objects.filter(dob=dob, applicationNo=stuId).values())
+            print(dat)
+            if not dat:
+                return redirect("home")
+            print(dat[0]['deportment_id'])
+            deportment = list(Deportment.objects.filter(id=dat[0]['deportment_id']).values("name"))
+            print(deportment[0]['name'])
+            pedagogy = list(Pedagogy.objects.filter(id=dat[0]['pedagogy_id']).values("subject"))
+            print(pedagogy[0]['subject'])
+            return render(request,"marklist.html",{"data":dat[0],"deportment":deportment[0]['name'],"pedagogy":pedagogy[0]['subject']})
+        return redirect("home")
     return redirect("home") 
