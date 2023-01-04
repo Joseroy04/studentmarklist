8bc8b68389ecc3697b6caa11665026d605f2d729
Joseroy04 <88181888+Joseroy04@users.noreply.github.com>
2023-01-04 18:33:39 +0530

    all changes

--- app1/admin.py
+++ app1/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from  .models import studentData,Pedagogy
+from  .models import studentData,Pedagogy,Deportment
 
 admin.site.register(studentData)
 admin.site.register(Pedagogy)
+admin.site.register(Deportment)
