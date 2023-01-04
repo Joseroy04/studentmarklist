d8ac2c4c14f7a7a73711af64832ad1685a8bdd15
Joseroy04 <88181888+Joseroy04@users.noreply.github.com>
2023-01-04 14:40:40 +0530

    error fixed on whitenoice

--- studentmarklist/settings.py
+++ studentmarklist/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1','whitenoise.middleware.WhiteNoiseMiddleware'
+    'app1',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'studentmarklist.urls'
